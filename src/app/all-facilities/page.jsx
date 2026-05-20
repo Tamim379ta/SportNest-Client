1243595ec8387ae84a6ce23c483599e2ce7628af
Tamim379ta <tamimthasan55@gmail.com>
2026-05-21 01:27:26 +0600

    added search and category

--- src/app/all-facilities/page.jsx
+++ src/app/all-facilities/page.jsx
@@ -1,18 +1,65 @@
-import FacilityCard from '@/components/shared/FacilityCard';
-import React from 'react';
+"use client";
 
-const AllFacilitiesPage = async () => {
-  const res = await fetch('http://localhost:5000/all-facilities')
-  const data = await res.json()
+import { useEffect, useState } from "react";
+import FacilityCard from "@/components/shared/FacilityCard";
+import Search from "@/components/shared/Search";
+import CategoryDropdown from "@/components/shared/SortBy";
+
+const AllFacilitiesPage = () => {
+
+  const [facilities, setFacilities] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchText, setSearchText] = useState("");
+
+
+
+  const fetchFacilities = async (text = searchText, category = selectedCategory) => {
+
+    const res = await fetch(
+      `http://localhost:5000/all-facilities?search=${text}&category=${category}`
+    );
+
+    const data = await res.json();
+    setFacilities(data);
+  };
+  useEffect(() => {
+    fetchFacilities();
+  }, []);
+
   return (
-    <div className='pt-20'>
-      <h1 className='font-bold text-3xl text-center pt-5'>All Facilitis</h1>
+    <div className="pt-25">
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 container mx-auto my-10'>
-        {
-          data.map(facility => <FacilityCard key={facility._id} facility={facility} />)
-        }
+      <h1 className="text-center text-3xl font-bold">
+        All Facilities
+      </h1>
+
+      <div className="container mx-auto mt-5">
+
+        <Search
+          onSearch={(text) => {
+            setSearchText(text);
+            fetchFacilities(text, selectedCategory);
+          }}
+        />
       </div>
+      <div className="container mx-auto flex justify-end mt-2">
+        <CategoryDropdown
+          onChange={(value) => {
+            setSelectedCategory(value);
+            fetchFacilities(searchText, value);
+          }}
+        />
+      </div>
+
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 container mx-auto mt-5 mb-10">
+
+        {facilities.map((f) => (
+          <FacilityCard key={f._id} facility={f} />
+        ))}
+
+      </div>
+
     </div>
   );
 };
